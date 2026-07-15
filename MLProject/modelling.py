1a8edea9159b7cc318a7360c1b91e8bd2a222dca
Alvin Saputra <laodealvinrahmats@gmail.com>
2026-07-16 01:50:34 +0800

    Initial MLProject

---
+++ MLProject/modelling.py
@@ -0,0 +1,56 @@
+import mlflow
+import mlflow.sklearn
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+mlflow.set_experiment("Telco_Churn")
+
+
+mlflow.sklearn.autolog()
+
+
+df = pd.read_csv("telco_preprocessed.csv")
+
+
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+
+with mlflow.start_run():
+
+    model = LogisticRegression(max_iter=1000)
+
+    model.fit(X_train, y_train)
+
+    pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, pred)
+    precision = precision_score(y_test, pred)
+    recall = recall_score(y_test, pred)
+    f1 = f1_score(y_test, pred)
+
+    print("=" * 40)
+    print("Accuracy :", acc)
+    print("Precision:", precision)
+    print("Recall   :", recall)
+    print("F1 Score :", f1)
+    print("=" * 40)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
